c69205be96ace1dca35ea49a8af42b96285be503
aoru_k <k.sk8.clgr@gmail.com>
2025-06-11 23:32:26 +0900

    コンソールでイベント確認

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-function getElement() {
-    elements = document.querySelectorAll('.shop li');
-    //※lenはlengthの略
-    let len = elements.length;
-    for (let i = 0; i < len; i++){
-        console.log(elements.item(i).textContent);
-    }
-}
+let lists = document.getElementsByClassName("pull-down-list")
+console.log(lists)
+
