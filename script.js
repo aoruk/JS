672adfd3c411c76ec691ef26ab0ee54577c6a7f7
aoru_k <k.sk8.clgr@gmail.com>
2025-06-11 21:14:18 +0900

    document.querySelectorAll①
    document.querySelectorAll②

---
+++ script.js
@@ -0,0 +1,6 @@
+const listItem = document.getElementById("lists");
+
+function getElement() {
+    element = document.querySelector('#shopB');
+    console.log('選ばれたのは' + element.textContent + 'でした');
+}
