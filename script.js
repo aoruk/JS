0b84dfc0b85eae7f534b4ddef4f32b2d1e617150
aoru_k <k.sk8.clgr@gmail.com>
2025-06-18 22:44:55 +0900

    イベント発火　基礎編

---
+++ script.js
@@ -0,0 +1,17 @@
+const pullDownButton = document.getElementById("lists");
+console.log(pullDownButton);
+
+window.addEventListener('load', function() {
+    const pullDownButton = document.getElementById("lists");
+
+    pullDownButton.addEventListener('mouseover', function(){
+        console.log('乗る');
+    });
+    pullDownButton.addEventListener('mouseout', function(){
+        console.log('外れる');
+    });
+    pullDownButton.addEventListener('click', function(){
+        console.log('クリック');
+    });
+  });
+ 
