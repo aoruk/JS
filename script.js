08e0c99b2062dd5480aff5aed865d4301c8b80a1
aoru_k <k.sk8.clgr@gmail.com>
2025-07-03 16:55:13 +0900

    js11 条件分岐編

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const pullDownButton = document.getElementById("lists");
-console.log(pullDownButton);
+const pullDownParents = document.getElementById("pull-down");
+
 
 window.addEventListener('load', function() {
     const pullDownButton = document.getElementById("lists");
@@ -16,13 +17,21 @@ window.addEventListener('load', function() {
   });
  
 pullDownButton.addEventListener('mouseover',function(){
-    pullDownButton.setAttribute("style","background-color:blue;")
+    this.setAttribute("style","background-color:blue;")
+    console.log('乗ったら青色')
 })
 
 pullDownButton.addEventListener('mouseout',function(){
-    pullDownButton.setAttribute("style","background-color:red;")
+    this.setAttribute("style","background-color:red;")
+    console.log('外れたら赤色')
 })
 
 pullDownButton.addEventListener('click',function(){
-    pullDownButton.setAttribute("style","background-color:green;")
+    if (pullDownParents.getAttribute("style") == "display:block;"){
+      pullDownParents.removeAttribute("style", "display:block;")
+      console.log("非表示")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+      console.log("表示")
+    }
 })
